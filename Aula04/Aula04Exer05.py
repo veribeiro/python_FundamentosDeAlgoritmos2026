f7f188f3c44c3c7c1fd9eda74adb03c21f094593
Verônica <veronica.rib16@gmail.com>
2026-03-05 16:33:00 -0300

    ultimo exercicio da aula 4

--- Aula04/Aula04Exer05.py
+++ Aula04/Aula04Exer05.py
@@ -4,4 +4,19 @@
 codigo = int(input("Escreva o código do produto: "))
 
 if codigo==1:
-    print("Produto")
+    print("Alimento não perecível")
+
+elif codigo==2 or codigo==3 or codigo==4:
+    print("Alimento perecível")
+
+elif codigo==5 or codigo==6:
+    print("Vestuário")
+
+elif codigo==7:
+    print("Higiene pessoal")
+
+elif codigo>=8 and codigo<=15:
+    print("Limpeza e utensílios domésticos")
+
+else:
+    print("Inválido")
